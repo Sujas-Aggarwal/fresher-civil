d8fb7ac4ad0986959cbfe9a5c4937926213ed839
Sujas <ce1221047@iitd.ac.in>
2024-08-25 17:27:24 +0530

    :v

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,6 @@
 function Hero() {
     return (
-        <div className="flex justify-center items-center w-full h-screen">
+        <div className="flex justify-center items-center w-full h-screen relative overflow-hidden">
             <video
                 src="https://video-public.canva.com/VAFKkbUWLwU/v/3484504a00.mp4?bandwidth=2935444&resolution=4000"
                 autoPlay
