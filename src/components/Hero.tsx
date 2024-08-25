2b7a43a240caaac1c1ae78d5b7eab08c10c14ac2
Sujas <ce1221047@iitd.ac.in>
2024-08-25 17:26:32 +0530

    :v

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,7 +8,7 @@ function Hero() {
                 loop
                 className="absolute top-0 left-0 -z-20 w-full h-full object-cover"
             ></video>
-            <div className="flex justify-center items-center flex-col w-full h-full p-6 bg-black/60 rounded-lg">
+            <div className="flex justify-center items-center flex-col w-full h-full p-6 bg-black/60">
                 <h1 className="text-white text-4xl text-center font-bold animation-text">
                     Fresher Party <br /> <p className="animation-text">2024</p>
                 </h1>
